420f28acf3c264004658ab5b09e41e6bcc80c3e8
Alina Orlov <alinaorlov@Alinas-MacBook-Pro.local>
2024-01-09 20:37:10 -0500

    new website

---
+++ script.js
@@ -0,0 +1,31 @@
+    // Get the current URL path without the domain
+    var path = window.location.pathname;
+    // Get the filename only
+    var page = path.split("/").pop();
+
+    // Select all navigation links and dropdown options
+    var navLinks = document.querySelectorAll('.navigation a');
+    var dropdownOptions = document.querySelector('.dropdown-nav').options;
+
+    // Loop over each link in the navigation
+    navLinks.forEach(function(link) {
+        if(link.getAttribute('href') === page) {
+            link.classList.add('active');
+        }
+    });
+
+    // Loop over each option in the dropdown
+    for (var i = 0; i < dropdownOptions.length; i++) {
+        if (dropdownOptions[i].value === page) {
+            dropdownOptions[i].selected = true;
+            break;
+        }
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var header = document.querySelector('.header');
+        header.style.cursor = 'pointer';
+        header.addEventListener('click', function() {
+            window.location.href = 'index.html';
+        });
+    });
